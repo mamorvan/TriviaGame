8ade207ccf1a0a9669c3e0bebc681564cfd9dbc2
Mary Morvan <mamorvan@yahoo.com>
2017-03-16 18:00:59 -0400

    add question objects

---
+++ assets/javascript/app.js
@@ -0,0 +1,94 @@
+//GLOBAL VARIABLES//
+var question1 = {
+	question: "Who was the Ancient Egyptian sun god?",
+	choice1: "Ra",
+	choice2: "Osiris",
+	choice3: "Anubis",
+	choice4: "Rameses",
+	image: "find pic of Ra"
+}
+
+var question2 = {
+	question: "Which king united Upper and Lower Egypt in 2900 B.C. and became the first pharoah?",
+	choice1: "Rameses I",
+	choice2: "Narmer",
+	choice3: "Anubis",
+	choice4: "Ceasar",
+	image: "find palette of Narmer"
+}
+
+var question3 = {
+	question: "What was the most common drink in Ancient Egypt?",
+	choice1: "Water",
+	choice2: "Wine",
+	choice3: "Milk",
+	choice4: "Beer",
+	image: "image of brewing or workers"
+}
+
+var question4 = {
+	question: "Why did Ancient Egyptian men and women wear kohl on their eyes?",
+	choice1: "It was a symbol of devotion to Ra",
+	choice2: "It was considered a part of traditional dress",
+	choice3: "It helped reduce glare from the sun",
+	choice4: "It was a symbol of great wealth",
+	image: "nefertiti bust"
+}
+
+var question5 = {
+	question: "Who was Isis?",
+	choice1: "A Roman empress who lived in Ancient Egypt",
+	choice2: "The wife of Rameses II",
+	choice3: "The protectress of Egypt and wife of Osiris",
+	choice4: "The daughter of Cleopatra and Ceasar",
+	image: "find pic of Isis"
+}
+
+var question6 = {
+	question: "What was stored in canopic jars?",
+	choice1: "grain",
+	choice2: "wine",
+	choice3: "kohl",
+	choice4: "organs",
+	image: "find pic of canopic jars"
+}
+
+var question7 = {
+	question: "What happened to the brain during mummification?",
+	choice1: "It was extracted through the nose and thrown away",
+	choice2: "It was mummified with the rest of the body",
+	choice3: "It was stored in a canopic jar with a baboon head top",
+	choice4: "It was taken out of the skull and dried out in the sun",
+	image: "find image of mummy"
+}
+
+var question8 = {
+	question: "Which canopic jar was the stomach stored in during mummification?",
+	choice1: "jackal-headed Duamutef",
+	choice2: "human-headed Imsety",
+	choice3: "baboon-headed Hapi",
+	choice4: "falcon-headed Qebehsenuef",
+	image: "find image of jackal-headed"
+}
+
+var question9 = {
+	question: "Who was the Ancient Egyptian goddess of cats and home?",
+	choice1: "Sekhmet",
+	choice2: "Thoth",
+	choice3: "Hathor",
+	choice4: "Bastet",
+	image: "find pic of Bastet"
+}
+
+var question10 = {
+	question: "Who was the first female pharoah?",
+	choice1: "Nefertiti",
+	choice2: "Cleopatra",
+	choice3: "Hatshepsut",
+	choice4: "There was never a female pharoah",
+	image: "pic of Hatshepsut"
+}
+
+//FUNCTIONS//
+
+//MAIN PROCESS//
